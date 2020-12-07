70c31e6e59a510af9f273dd71f789ee9b3f08b73
UnusualProgrammist <75139757+UnusualProgrammist@users.noreply.github.com>
2020-12-07 18:08:59 +0100

    Tidying code

--- src/com/camera/classesAndPrograms/CameraStream.java
+++ src/com/camera/classesAndPrograms/CameraStream.java
@@ -13,19 +13,19 @@ import java.nio.file.FileSystems;
 import java.nio.file.Path;
 
 public class CameraStream extends Application {
-    private VideoCapture capture;
 
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage){
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
         Path videoPath = FileSystems.getDefault().getPath("necessaryFiles" + File.separator + "dashCam.mp4"); //Cars
         Path haarcascadePath = FileSystems.getDefault().getPath("necessaryFiles" + File.separator + "haarcascadeForCars.xml"); //xml file
+        // Setting up stage
         ImageView imageView = new ImageView();
         HBox hbox = new HBox(imageView);
         Scene scene = new Scene(hbox);
         stage.setScene(scene);
         stage.show();
-        ObjectDetector objectDetector = new ObjectDetector(videoPath, haarcascadePath );
+        ObjectDetector objectDetector = new ObjectDetector(videoPath, haarcascadePath ); // constractor with paths to video and xml file
         new AnimationTimer(){
             @Override
             public void handle(long l) {
